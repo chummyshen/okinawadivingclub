87ef7e023d5d192ef0532f131c932eef4b0090df
chummyshen <113606112+chummyshen@users.noreply.github.com>
2022-09-15 21:52:55 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="沖縄潜水俱楽部　!" />
+        <Header title="沖縄潜水俱楽部" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+         Coming Soon! <code>pages/index.js</code>
         </p>
       </main>
 
